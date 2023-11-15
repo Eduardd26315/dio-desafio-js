fbe77b8d0f31b0d46e0f35eb9faaaec77dda6aa1
eduard <edup4801@gmail.com>
2023-11-15 12:27:09 -0300

    adicionando verificadore no while

--- projeto.js
+++ projeto.js
@@ -1,37 +1,56 @@
-let nome = prompt("Qual é o seu nome de herói?");
+let nome;
 let quantiaDeXp;
+
+while (true) {
+    nome = prompt("Qual é o seu nome de herói?");
+
+    if (/^[A-Za-z]+$/.test(nome)) {
+        break;
+    } else {
+        alert("Por favor, insira um nome válido contendo apenas letras.");
+    }
+}
 
 while (true) {
     quantiaDeXp = Number(prompt("Qual é o seu nível de XP?"));
 
-    if (!isNaN(quantiaDeXp)) {
+    if (!isNaN(quantiaDeXp) && Number.isInteger(quantiaDeXp) && quantiaDeXp > 0) {
         break;
     } else {
-        alert("Por favor, insira um valor numérico válido para a experiência.");
+        alert("Por favor, insira um valor numérico inteiro e positivo válido para a experiência.");
     }
 }
 
 let nivel;
 
-if (quantiaDeXp < 1000) {
-    nivel = "Ferro";
-} else if (quantiaDeXp >= 1001 && quantiaDeXp <= 2000) {
-    nivel = "Bronze";
-} else if (quantiaDeXp >= 2001 && quantiaDeXp <= 5000) {
-    nivel = "Prata";
-} else if (quantiaDeXp >= 6001 && quantiaDeXp <= 7000) {
-    nivel = "Ouro";
-} else if (quantiaDeXp >= 7001 && quantiaDeXp <= 8000) {
-    nivel = "Platina";
-} else if (quantiaDeXp >= 8001 && quantiaDeXp <= 9000) {
-    nivel = "Ascendente";
-} else if (quantiaDeXp >= 9001 && quantiaDeXp <= 10000) {
-    nivel = "Imortal";
-} else {
-    nivel = "Radiante";
+switch (true) {
+    case quantiaDeXp < 1000:
+        nivel = "Ferro";
+        break;
+    case quantiaDeXp <= 2000:
+        nivel = "Bronze";
+        break;
+    case quantiaDeXp <= 5000:
+        nivel = "Prata";
+        break;
+    case quantiaDeXp <= 7000:
+        nivel = "Ouro";
+        break;
+    case quantiaDeXp <= 8000:
+        nivel = "Platina";
+        break;
+    case quantiaDeXp <= 9000:
+        nivel = "Ascendente";
+        break;
+    case quantiaDeXp <= 10000:
+        nivel = "Imortal";
+        break;
+    default:
+        nivel = "Radiante";
 }
 
 alert(`O Herói de nome ${nome} está no nível de ${nivel}`);
+
 
 
 
